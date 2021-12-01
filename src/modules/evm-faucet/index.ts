40f35f9d5b9bdca79f3ae54e511f4a099e26854a
impelcrypto <impelcrypto@gmail.com>
2021-12-01 23:02:25 +0800

    fix: define evm endpoint

--- src/modules/evm-faucet/index.ts
+++ src/modules/evm-faucet/index.ts
@@ -1,8 +1,14 @@
 export { evmFaucet, generateFaucetId } from './utils';
+
 export enum Network {
     shiden = 'shiden',
     shibuya = 'shibuya',
 }
+
+export const evmEndpoint = {
+    [Network.shiden]: 'https://rpc.shiden.astar.network:8545',
+    [Network.shibuya]: 'https://rpc.shibuya.astar.network:8545',
+};
 export type ChainNetwork = Network.shibuya | Network.shiden;
 
 export const FAUCET_AMOUNT = '0.002';
--- src/modules/web3/index.ts
+++ src/modules/web3/index.ts
@@ -1,12 +1,7 @@
+import { evmEndpoint } from './../evm-faucet/index';
 import Web3 from 'web3';
 import { Network } from '../evm-faucet';
 
 export const createWeb3Instance = (network: Network) => {
-    return new Web3(
-        new Web3.providers.HttpProvider(
-            network === Network.shiden
-                ? 'https://rpc.shiden.astar.network:8545'
-                : 'https://rpc.shibuya.astar.network:8545',
-        ),
-    );
+    return new Web3(new Web3.providers.HttpProvider(evmEndpoint[network]));
 };
